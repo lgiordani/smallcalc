975bd11ac88dcf2ee487a7deb1c4636072bf003f
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-04-18 20:01:07 +0100

    CLI: parse_integer() is the new entry point

--- cli.py
+++ cli.py
@@ -1,15 +1,27 @@
+from smallcalc import calc_parser as cpar
+from smallcalc import calc_visitor as cvis
+
+
 def main():
+    p = cpar.CalcParser()
+    v = cvis.CalcVisitor()
+
     while True:
         try:
             text = input('smallcalc :> ')
+            p.lexer.load(text)
+
+            node = p.parse_integer()
+            res = v.visit(node.asdict())
+
+            print(res)
+
         except EOFError:
             print("Bye!")
             break
 
         if not text:
             continue
-
-        print(text)
 
 
 if __name__ == '__main__':
