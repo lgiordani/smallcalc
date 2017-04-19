ba7d54357567fad3da8b9992f5e3c7313dd38d0d
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-04-19 22:53:45 +0100

    Lexer: multiple operations are correctly handled

--- tests/test_calc_lexer.py
+++ tests/test_calc_lexer.py
@@ -85,3 +85,18 @@ def test_get_tokens_understands_subtraction():
         token.Token(clex.EOF)
     ]
 
+
+def test_get_tokens_understands_multiple_operations():
+    l = clex.CalcLexer()
+
+    l.load('3 + 5 - 7')
+
+    assert l.get_tokens() == [
+        token.Token(clex.INTEGER, '3'),
+        token.Token(clex.LITERAL, '+'),
+        token.Token(clex.INTEGER, '5'),
+        token.Token(clex.LITERAL, '-'),
+        token.Token(clex.INTEGER, '7'),
+        token.Token(clex.EOL),
+        token.Token(clex.EOF)
+    ]
