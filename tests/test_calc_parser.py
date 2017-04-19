92f889ffaf09e183fb150b7b72f7323a0fbcb8cf
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-04-19 22:12:50 +0100

    Parser: minus sign accepted

--- tests/test_calc_parser.py
+++ tests/test_calc_parser.py
@@ -34,3 +34,26 @@ def test_parse_expression():
             'value': '+'
         }
     }
+
+
+def test_parse_expression_understands_subtraction():
+    p = cpar.CalcParser()
+    p.lexer.load("2-3")
+
+    node = p.parse_expression()
+
+    assert node.asdict() == {
+        'type': 'binary',
+        'left': {
+            'type': 'integer',
+            'value': 2
+        },
+        'right': {
+            'type': 'integer',
+            'value': 3
+        },
+        'operator': {
+            'type': 'literal',
+            'value': '-'
+        }
+    }
