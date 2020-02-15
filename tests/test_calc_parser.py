45ca8f8ba7891aba85795a3fa3a072534a0d9452
Leonardo Giordani <giordani.leonardo@gmail.com>
2020-02-15 15:58:47 +0000

    Parser: assignment support expressions

--- tests/test_calc_parser.py
+++ tests/test_calc_parser.py
@@ -346,3 +346,68 @@ def test_parse_assignment():
             'value': 5
         }
     }
+
+
+def test_parse_assignment_with_expression():
+    p = cpar.CalcParser()
+    p.lexer.load("x = 4 * (3 + 5)")
+
+    node = p.parse_assignment()
+
+    assert node.asdict() == {
+        'type': 'assignment',
+        'variable': 'x',
+        'value': {
+            'type': 'binary',
+            'operator': {
+                'type': 'literal',
+                'value': '*'
+            },
+            'left': {
+                'type': 'integer',
+                'value': 4
+            },
+            'right': {
+                'type': 'binary',
+                'operator': {
+                    'type': 'literal',
+                    'value': '+'
+                },
+                'left': {
+                    'type': 'integer',
+                    'value': 3
+                },
+                'right': {
+                    'type': 'integer',
+                    'value': 5
+                }
+            }
+        }
+    }
+
+
+def test_parse_assignment_expression_with_variables():
+    p = cpar.CalcParser()
+    p.lexer.load("x = y + 4")
+
+    node = p.parse_assignment()
+
+    assert node.asdict() == {
+        "type": "assignment",
+        "variable": "x",
+        'value': {
+            'type': 'binary',
+            'operator': {
+                'type': 'literal',
+                'value': '+'
+            },
+            'left': {
+                'type': 'variable',
+                'value': 'y'
+            },
+            'right': {
+                'type': 'integer',
+                'value': 4
+            },
+        }
+    }
